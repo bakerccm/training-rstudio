2d8cb93b9137b62a95c4ed0a91fc4353c5bdc458
Chris Baker <21959506+bakerccm@users.noreply.github.com>
2021-05-20 08:51:21 -0400

    add indentation and clean up commands

--- workshop_commands.sh
+++ workshop_commands.sh
@@ -2,95 +2,101 @@
 
 ## IMPORTANT: Run one line at a time! 
 
-## SET UP
-# set up conda and initialize settings
-conda init
+## INITIALIZE CONDA
 
-# shorten command prompt to $
-echo "PS1='\w $ '" >> .bashrc
+  # set up conda and initialize settings
+  conda init
 
-# re-start terminal for the changes to take effect (i.e., type 'exit' and then open a new terminal)
+  # shorten command prompt to $
+  echo "PS1='\w $ '" >> .bashrc
 
-# configure conda channels
-conda config --show channels
+  # re-start terminal for the changes to take effect (i.e., type 'exit' and then open a new terminal)
 
-# list packages in current environment
-conda list
+## CONDA CHANNELS
 
-# add defaults channel
-conda config --add channels defaults
-# check channel order
-conda config --get channels
-# add bioconda channel
-conda config --add channels bioconda
-# check channel order
-conda config --get channels
-# add conda-forge channel
-conda config --add channels conda-forge
-# check channel order
-conda config --get channels
+  # configure conda channels
+  conda config --show channels
 
-## INSTALL SOFTWARE and CREATE CONDA ENVIRONMENTS
-# search for software
-conda search fastqc
+  # list packages in current environment
+  conda list
 
-# create conda environment and install fastqc
-conda create -y --name fqc fastqc
+  # add defaults channel
+  conda config --add channels defaults
+  # check channel order
+  conda config --get channels
+  # add bioconda channel
+  conda config --add channels bioconda
+  # check channel order
+  conda config --get channels
+  # add conda-forge channel
+  conda config --add channels conda-forge
+  # check channel order
+  conda config --get channels
 
-# activate environment
-conda activate fqc
+## INSTALL FASTQ SOFTWARE and CREATE CONDA ENVIRONMENTS
 
-# check fastqc version
-fastqc --version
+  # search for software
+  conda search fastqc
 
-# Method 1: install software in existing environment
-conda install -y trimmomatic=0.36
-conda list # check installed software
+  # create conda environment and install fastqc
+  conda create -y --name fqc fastqc
 
-# Method 2: install both software during environment creation
-echo $PATH # check path for fqc environment
-conda deactivate
-conda create -y --name fqc_trim fastqc trimmomatic=0.36
-conda activate fqc_trim 
-conda list # check installed software
-echo $PATH # check path for fqc_trim environment
+  # activate environment
+  conda activate fqc
 
-# Method 3: specify software to install with a YAML file
-conda deactivate
-# click on fqc.yml in file directory to see how it is formatted
-conda env create -f fqc.yml #since environment name specified in yml file, we do not need to use -n flag here
-conda activate fqc_yaml
-conda list  # check installed software
+  # check fastqc version
+  fastqc --version
 
-# Look at conda environments 
-conda env list
+## INSTALL TRIMMOMATIC SOFTWARE DIFFERENT WAYS
+
+  # Method 1: install software in existing environment
+  conda install -y trimmomatic=0.36
+  conda list # check installed software
+
+  # Method 2: install both software during environment creation
+  echo $PATH # check path for fqc environment
+  conda deactivate
+  conda create -y --name fqc_trim fastqc trimmomatic=0.36
+  conda activate fqc_trim 
+  conda list # check installed software
+  echo $PATH # check path for fqc_trim environment
+
+  # Method 3: specify software to install with a YAML file
+  conda deactivate
+  # click on fqc.yml in file directory to see how it is formatted
+  conda env create -f fqc.yml #since environment name specified in yml file, we do not need to use -n flag here
+  conda activate fqc_yaml
+  conda list  # check installed software
+
+  # Look at conda environments 
+  conda env list # or ...
+  conda info --envs # same thing
 
 ## Running FastQC in the fqc Environment
-# activate environment
-conda activate fqc
 
-# check fastqc installed
-fastqc --help
+  # activate environment
+  conda activate fqc
 
-# Download data
-curl -L https://osf.io/5daup/download -o ERR458493.fastq.gz
+  # check fastqc installed
+  fastqc --help
 
-# Checkout the data!
-# line count
-gunzip -c ERR458493.fastq.gz | wc -l
+  # Download data
+  curl -L https://osf.io/5daup/download -o ERR458493.fastq.gz
 
-# top of file
-gunzip -c ERR458493.fastq.gz | head
+  # Check out the data!
+  # line count
+  zcat ERR458493.fastq.gz | wc -l
 
-## or use less to scroll through the file contents. type 'q' to exit the page
-less ERR458493.fastq.gz
+  # top of file
+  zcat ERR458493.fastq.gz | head
 
-# Run FastQC!
-fastqc ERR458493.fastq.gz
+  # Run FastQC!
+  fastqc ERR458493.fastq.gz
 
 ## CLEANING UP
-# to remove old conda environments (optional!)
-conda env remove --name fqc_trim
+
+  # to remove old conda environments (optional!)
+  conda env remove --name fqc_trim
 
 
 
